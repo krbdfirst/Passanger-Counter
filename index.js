a9afcbe9bbd7025196ed95c25a34ffd1c9170fc4
KRBdFirst <reshybella10@gmail.com>
2022-05-24 09:17:32 +0100

    count must not go below zero

--- index.js
+++ index.js
@@ -4,26 +4,15 @@ function increment() {
     click += 1;
     countEl.textContent = click;
     console.log(click)
-}
+} //function for increment
 
 function decrement() {
-    click -= 1
+    if (click > 0) {
+        click -= 1
+    }
     countEl.textContent = click;
-    console.log(click)
-}
+} //function for decrement
 
-
-let message = "You have 3 new notifications, "
-let username = "per !"
-
-let messageToUser = message + username
-console.log(messageToUser)
-let greetings = 'Have fun while Counting,'
-let myName = " Stranger !"
-
-let myGreeting = greetings + myName
-document.getElementById("greetings").textContent = myGreeting
-document.getElementById('greetings').textContent += "💙"
 
 let showThis = document.getElementById("logentry")
 
@@ -32,27 +21,4 @@ function save() {
     showThis.textContent += logs
     click = click - click
     countEl.textContent = click
-}
-let firstNam = 'Bello'
-let lastNam = 'Rasheed'
-let fullNam = firstNam + ' ' + lastNam
-console.log(fullNam)
-
-function logger() {
-    console.log(fullnam)
-}
-let myPoints = 3
-
-function add3Points() {
-    myPoints += 3
-}
-function remove1Point() {
-    myPoints -= 1
-}
-
-add3Points()
-add3Points()
-add3Points()
-remove1Point()
-remove1Point()
-console.log(myPoints)
+} //function to save the current count
